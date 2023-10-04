277a5a0e20026b02972c8cdd5b5498b8e9b53077
MMolieleng <nosichefane@gmail.com>
2023-10-04 18:18:16 +0200

    added login form

---
+++ src/app/components/LoginForm.tsx
@@ -0,0 +1,22 @@
+// components/SignupForm.js
+import React from 'react';
+
+const LoginForm = () => {
+  return (
+    <div className="signup-form-container">
+      <h2 className="header">Login</h2>
+      <form className="signup-form">
+        <div className="form-group">
+          <input type="text" id="username" name="username" placeholder="enter your username" />
+        </div>
+        <div className="form-group">
+          <input type="password" id="password" name="password" placeholder="enter password" />
+        </div>
+     
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default LoginForm;
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,11 +1,12 @@
 import Navbar from "../components/Navbar";
 import styles from "@/app/page.module.css";
+import LoginForm from "../components/LoginForm";
 
 export default function Login() {
     return  <>
     <Navbar/>
      <main className={styles.main}>
-     <h1>Login form will appear here</h1>
+    <LoginForm/>
    </main>
    </>
   }
