d67e4519d095325c4ecba360fc3b93536fd2c030
MMolieleng <nosichefane@gmail.com>
2023-10-05 19:04:18 +0200

    modified register controller

--- src/app/pages/api/models/register_model.ts
+++ src/app/pages/api/models/register_model.ts
@@ -4,7 +4,6 @@ const UserSchema = new mongoose.Schema({
   username: {
     type: String,
     required: [true, 'Please add a username'],
-    maxlength: [3, 'user cannot be more than 40 characters'],
   },
   email: {
     type: String,
